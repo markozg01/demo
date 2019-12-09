ef7b7f1ff64127bb4f5d163f3a21411c8dc02ed4
markozg01 <marko.zg01@gmail.com>
2019-12-09 11:08:31 +0100

    - version for prod

--- src/main/groovy/com/example/demo/DemoApplication.groovy
+++ src/main/groovy/com/example/demo/DemoApplication.groovy
@@ -9,6 +9,8 @@ class DemoApplication {
 	static void main(String[] args) {
 		SpringApplication.run(DemoApplication, args)
 		//Aa
+		//ovo je sad novo na masteru 1
+		//ovo je sad novo na masteru 2
 	}
 
 }
