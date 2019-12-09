14b76f94e1fde2a4e023a5528e09789203572782
markozg01 <marko.zg01@gmail.com>
2019-12-09 11:10:22 +0100

    Revert "mm"
    This reverts commit 310634c3279d4d5a853d4ebe1c9c4627de011d6c.

--- src/main/groovy/com/example/demo/DemoApplication.groovy
+++ src/main/groovy/com/example/demo/DemoApplication.groovy
@@ -9,10 +9,6 @@ class DemoApplication {
 	static void main(String[] args) {
 		SpringApplication.run(DemoApplication, args)
 		//Aa
-		//ovo smo dodali u branchu
-		//još i ovo
-		//ovo je sad novo na masteru 1
-		//ovo je sad novo na masteru 2
 	}
 
 }
